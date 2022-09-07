4332fa44a7438a90ffb1a941af1cb8daf362a92e
Matt <matt.hong@adriel.com>
2022-09-07 09:53:43 +0900

    added matt music player linked list

---
+++ LinkedList/Matt/MusicList.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    name: "그때 그 순간 그대로 (그그그)",
+    artist: "WSG워너비(가야G)",
+  },
+  {
+    name: "없었던 일로",
+    artist: "헤이즈 (Heize)아티스트명",
+  },
+  {
+    name: "보고싶었어",
+    artist: "WSG워너비(4FIRE)아티스트명",
+  },
+  {
+    name: "고백하는 취한밤에 (Prod. 2soo)",
+    artist: "임재현",
+  },
+  {
+    name: "흔한 거짓말",
+    artist: "정승환",
+  },
+  {
+    name: "Left and Right (Feat. Jung Kook of BTS)",
+    artist: "Charlie Puth, 정국, 방탄소년단",
+  },
+  {
+    name: "Kids Are Born Stars",
+    artist: "Lauv",
+  },
+  {
+    name: "Alone (Feat. 이하이)",
+    artist: "쿠기(Coogie)",
+  },
+  {
+    name: "Take Me (with 11키티즈)",
+    artist: "코드쿤스트, meenoi (미노이)",
+  },
+  {
+    name: "Hot Shit (Feat. Kanye West & Lil Durk)",
+    artist: "Cardi B",
+  },
+];
