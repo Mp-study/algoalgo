6e61e1454d89727ebe5ae7ce67fecbdd32b95be4
Matt <matt.hong@adriel.com>
2022-10-05 13:16:22 +0900

    Matt added binary search

---
+++ Matt/BinarySearch.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+  let mid;
+
+  while (start <= end) {
+    mid = parseInt((start + end) / 2);
+
+    if (target === nums[mid]) {
+      return mid;
+    }
+
+    if (target < nums[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return -1;
+};
