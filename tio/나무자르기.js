c2aaf399fadac4769d93ceda0dca1042f987a884
Tio Lim <tio.lim@adriel.ai>
2022-10-08 16:30:43 +0900

    misc

--- tio/나무자르기.js
+++ tio/나무자르기.js
@@ -35,50 +35,50 @@
 //     console.log(s);
 // };
 
-// const input = [];
-// require("readline")
-//     .createInterface(process.stdin, process.stdout)
-//     .on("line", (line) => {
-//         input.push(line);
-//     })
-//     .on("close", () => {
-//         solution(input);
-//         process.exit();
-//     });
+const input = [];
+require("readline")
+    .createInterface(process.stdin, process.stdout)
+    .on("line", (line) => {
+        input.push(line);
+    })
+    .on("close", () => {
+        solution(input);
+        process.exit();
+    });
 
-//     const solution = ([condition, trees]) => {
-//         const [T, M] = condition.split(' ').map(Number);
-//         trees = trees.split(' ').map(Number);
+    const solution = ([condition, trees]) => {
+        const [T, M] = condition.split(' ').map(Number);
+        trees = trees.split(' ').map(Number);
         
-//         function f(sum, target) {
-//             return sum >= target;
-//         }
+        function f(sum, target) {
+            return sum >= target;
+        }
         
-//         let s = 0;
-//         let e = 1000000000;
-//         let ans = -1;
+        let s = 0;
+        let e = 1000000000;
+        let ans = -1;
         
-//         while (s <= e) {
-//             const m = Math.floor((s + e) / 2);
+        while (s <= e) {
+            const m = Math.floor((s + e) / 2);
 
-//             let sum = 0;
-//             for (const h of trees) {
-//                 sum += Math.max(0, h - m);
-//             }
+            let sum = 0;
+            for (const h of trees) {
+                sum += Math.max(0, h - m);
+            }
             
-//             if (f(sum, M)) {
-//                 if (ans === -1 || ans < m)
-//                     ans = m;            
-//                 s = m + 1;
-//             } else {
-//                 e = m - 1;                            
-//             }
-//         }
-//         if (ans === - 1) 
-//             return console.log(0);
+            if (f(sum, M)) {
+                if (ans === -1 || ans < m)
+                    ans = m;            
+                s = m + 1;
+            } else {
+                e = m - 1;                            
+            }
+        }
+        if (ans === - 1) 
+            return console.log(0);
         
-//         console.log(ans);
-//     };
+        console.log(ans);
+    };
 
 const input = [];
 require("readline")
