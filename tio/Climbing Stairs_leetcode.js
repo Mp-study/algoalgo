01475df8e4370d7683aa9a78df915ee964a24719
Tio Lim <tio.lim@adriel.ai>
2022-10-12 10:25:50 +0900

    ADD another climbing stairs

--- tio/Climbing Stairs_leetcode.js
+++ tio/Climbing Stairs_leetcode.js
@@ -3,7 +3,31 @@
  * @return {number}
  */
 
-// O(N * N!) O(N + M)
+const memo = {'1': 1, '2': 2};
+var climbStairs = function(n) {
+    if (n < 3) return n;
+    if (memo[n] !== undefined) return memo[n];
+
+    let first = 1;
+    let second = 2;
+
+    let count = 2;
+    while (true) {
+        if (count === n)
+            break;
+        
+        const temp = second;
+        second += first;
+        first = temp;
+        count++;
+        memo[count] = second;
+    }
+
+    return second;
+};
+
+
+// O(2^N) O(N + M)
 const memo = {};
 var climbStairs = function(n) {
     return countWays(n);
@@ -13,7 +37,7 @@ function countWays(n) {
     if (n === 1 || n === 2) {
         return n;
     }
-    if (memo[n] !== undefined) 
+    if (memo[n] !== undefined)
         return memo[n];
 
     const count = countWays(n - 1) + countWays(n - 2);
